5ee479c4f3028b356f8c0888c8219340eafa3f78
Mahimul Islam <mahimulislam@gmail.com>
2021-03-08 12:07:28 -0500

    fixedprocfiles

--- django_forms/urls.py
+++ django_forms/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
 from django.conf.urls.static import static
-from gettingstarted import settings
 from django.conf.urls import url
+
+from django_forms.django_forms import settings
 
 urlpatterns = [
     path('',include('myapp.urls')),
