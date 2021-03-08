0f341bea369982e170d867c961ce1927469fa634
Mahimul Islam <mahimulislam@gmail.com>
2021-03-08 12:29:05 -0500

    fixedsettingss

--- django_forms/settings.py
+++ django_forms/settings.py
@@ -55,9 +55,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'django_forms.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+    {'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
